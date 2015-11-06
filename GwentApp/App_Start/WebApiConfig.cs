1150ea04b9da3e73005cf9041c7b000fdd812d8b
otmshank00 <will.sykes@csus.edu>
2015-11-06 08:39:11 -0800

    Split deck create into modular setup. Created new web services calls that utilize the new deck builder

--- GwentApp/App_Start/WebApiConfig.cs
+++ GwentApp/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace GwentApp
 {
@@ -10,6 +12,59 @@ namespace GwentApp
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            // Force all Web API requests to return just JSON (we hate XML).
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+            // Enable CORS.
+            config.EnableCors();
+
+            // Custom routes for Card API Controller
+
+            config.Routes.MapHttpRoute(
+                name: "GetAllCardsService",
+                routeTemplate: "card/all",
+                defaults: new { controller = "Card", action = "GetAllCards" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "GetFactionsService",
+                routeTemplate: "card/factions",
+                defaults: new { controller = "Card", action = "GetAllFactions" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "GetLeadersService",
+                routeTemplate: "card/leaders",
+                defaults: new { controller = "Card", action = "GetAllLeaders" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "GetByPowerService",
+                routeTemplate: "card/power/{power}",
+                defaults: new { controller = "Card", action = "GetByPower" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "GetLeadersByFactionAbbreviationService",
+                routeTemplate: "card/leaders/byfaction/{factionAbbreviation}",
+                defaults: new { controller = "Card", action= "GetLeadersByFactionAbbreviation" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "GetLeaderByNameService",
+                routeTemplate: "card/leaders/byname/{leaderName}",
+                defaults: new { controller = "Card", action = "GetLeaderByName" }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "BuildDeckByFactionService",
+                routeTemplate: "card/builddeck/byfaction/{factionAbbreviation}",
+                defaults: new { controller = "Card", action = "BuildDeckByFaction" }
+
+            );
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ GwentApp/Controllers/CardController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GwentApp.Models;
+
+namespace GwentApp.Controllers
+{
+    public class CardController : ApiController
+    {
+        public IHttpActionResult GetAllCards ()
+        {
+            return Ok(GwentApp.Global.gAllCards);
+        }
+
+        public IHttpActionResult GetAllFactions()
+        {
+            return Ok(GwentApp.Global.gAllFactions);
+        }
+
+        public IHttpActionResult GetAllLeaders ()
+        {
+            return Ok(GwentApp.Global.gAllLeaders);
+        }
+
+        public IHttpActionResult GetByPower(int power)
+        {
+            IEnumerable<Card> cards = GwentApp.Global.gAllCards.Where(card => card.Power == power);
+            return Ok(cards);
+        }
+
+        public IHttpActionResult GetLeadersByFactionAbbreviation(string factionAbbreviation)
+        {
+            IEnumerable<LeaderInfo> leaders = GwentApp.Global.gAllLeaders.Where(leader => leader.LeaderFactionAbbr == factionAbbreviation);
+            return Ok(leaders);
+        }
+
+        public IHttpActionResult GetLeaderByName(string leaderName)
+        {
+            IEnumerable<LeaderInfo> leader = GwentApp.Global.gAllLeaders.Where(l => l.LeaderName == leaderName);
+            return Ok(leader);
+        }
+
+        [HttpGet]
+        public IHttpActionResult BuildDeckByFaction(string factionAbbreviation)
+        {
+            List<Card> deck = SetupController.BuildDeck(factionAbbreviation);
+            IEnumerable<Card> ieDeck = deck.AsEnumerable<Card>();
+            return Ok(ieDeck);
+        }
+    }
+}
